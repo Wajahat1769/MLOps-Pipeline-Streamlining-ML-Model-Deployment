30c3114f23a1d332556f5b2cb0df44313a848dbc
wajahat Ullah <i201769@nu.edu.pk>
2024-03-30 21:58:39 +0500

    formatting changed

--- app/utils.py
+++ app/utils.py
@@ -39,10 +39,16 @@ def preprocess_data(X):
     return preprocessed_X
 
 
-def split_data( X , y , test_size = 0.2 , random_state = 42 ):
+def split_data(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    ):
     """
     Split the dataset into training and testing sets.
     """
 
-    (X_train, X_test, y_train, y_test) = train_test_split(X, y,test_size=test_size, random_state=random_state)
+    (X_train, X_test, y_train, y_test) = train_test_split(X, y,
+            test_size=test_size, random_state=random_state)
     return (X_train, X_test, y_train, y_test)

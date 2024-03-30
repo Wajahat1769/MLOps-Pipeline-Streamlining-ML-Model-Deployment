8d4fa65c3322f6ae6ee8087ffd26a406c39c0e45
wajahat Ullah <i201769@nu.edu.pk>
2024-03-30 21:27:31 +0500

    Added model trained it and created flask API

---
+++ app/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+from model import RandomForestModel
+from utils import load_data, preprocess_data, split_data
+
+app = Flask(__name__)
+
+# Load the dataset
+X, y = load_data('D:/Assignment01-MLOPS/MLOps-Pipeline-Streamlining-ML-Model-Deployment/data/raw/wdbc.csv')
+
+# Preprocess the data
+X = preprocess_data(X)
+
+# Split the data into train and test sets
+X_train, X_test, y_train, y_test = split_data(X, y)
+
+# Train the model
+model = RandomForestModel()
+model.train(X_train, y_train)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    try:
+        # Assuming data is a dictionary containing feature-value pairs
+        # Extract numeric feature values from the dictionary
+        features = [float(value) for value in data.values()]
+        
+        # Make prediction using the model
+        prediction = model.predict([features])
+        return jsonify({'prediction': prediction.tolist()})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
---
+++ app/utils.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def load_data(data_path):
+    data = pd.read_csv(data_path)
+    X = data.iloc[:, 2:].values.astype(np.float32)  # Exclude first column (id) and second column (diagnosis)
+    y = data.iloc[:, 1].map({'M': 1, 'B': 0}).values  # Second column (diagnosis)
+    return X, y
+
+def preprocess_data(X):
+    """
+    Preprocess the dataset by applying necessary transformations.
+    """
+    # Separate the mean, standard error, and worst features
+    mean_features = X[:, :10]
+    std_error_features = X[:, 10:20]
+    worst_features = X[:, 20:]
+
+    # Normalize the features
+    scaler = StandardScaler()
+    mean_features = scaler.fit_transform(mean_features)
+    std_error_features = scaler.transform(std_error_features)
+    worst_features = scaler.transform(worst_features)
+
+    # Concatenate the preprocessed features
+    preprocessed_X = np.concatenate((mean_features, std_error_features, worst_features), axis=1)
+
+    return preprocessed_X
+
+def split_data(X, y, test_size=0.2, random_state=42):
+    """
+    Split the dataset into training and testing sets.
+    """
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    return X_train, X_test, y_train, y_test
